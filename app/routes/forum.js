f70070674d6d6cbb3939c79e3204004713fa7f1c
Ilya Makarov <dfrost.00@gmail.com>
2013-05-18 00:03:27 +0400

    course id for the topic schema

--- app/routes/forum.js
+++ app/routes/forum.js
@@ -14,6 +14,7 @@ var Schema = mongoose.Schema,
 var Topic = new Schema({
     title: { type: String, required: true },
     date: { type: Date, default: Date.now },
+    courseid: { type: String, required: true },
     author: { type: ObjectId, required: true }
 });
 
